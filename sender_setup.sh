6c41726237d5113401baed9edc6c8043f16ab47b
Kunwoo Kim <geonwoo.kim@navercorp.com>
2018-04-03 15:30:09 +0900

    done

--- sender_setup.sh
+++ sender_setup.sh
@@ -11,5 +11,5 @@ cd /root/NTM_MAKE_TRAFFIC
 curl https://bootstrap.pypa.io/get-pip.py | python3.6
 pip3.6 install -r requirements.txt
 
-echo "*/1 * * * * /usr/bin/python3.6 /root/NTM_MAKE_TRAFFIC/make_leased_line_traffic_private_ip_by_http.py > /tmp/internet1.log" >> /var/spool/cron/crontabs/root
-echo "*/1 * * * * /usr/bin/python3.6 /root/NTM_MAKE_TRAFFIC/make_leased_line_traffic_public_ip_by_http.py > /tmp/internet1.log" >> /var/spool/cron/crontabs/root
+echo "*/1 * * * * /usr/bin/python3.6 /root/NTM_MAKE_TRAFFIC/make_leased_line_traffic_private_ip_by_ping.py > /tmp/make_leased_line_traffic_private_ip_by_ping.log" >> /var/spool/cron/crontabs/root
+echo "*/1 * * * * /usr/bin/python3.6 /root/NTM_MAKE_TRAFFIC/make_leased_line_traffic_public_ip_by_ping.py > /tmp/make_leased_line_traffic_public_ip_by_ping.log" >> /var/spool/cron/crontabs/root
